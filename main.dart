03655d1b91259fe3ecce4c8ce8cbf4ef7ef4badd
Hunain225 <hunainnaveed225@gmail.com>
2022-10-16 17:02:13 +0500

    second

--- main.dart
+++ main.dart
@@ -1,3 +1,15 @@
- void main(){
- print("Hello world");
+void main(){
+  List Indexname = ["index1","index2","index3","index4","index5","index6"];
+   int one = 5;
+  if(Indexname.length >= one ) {
+    print("Your Index is valid");
+    print(Indexname[one]);
+
+  }
+  else{
+    print("Your Index is invalid");
+  }
+Indexname.replaceRange(2, 5, [12,13,14]);
+print(Indexname);
+
 }
